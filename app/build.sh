b49acb9f288b3b0a04ace6122d1e82da9af2e510
Sergij Lebedyn <lebedyn.sergij@gmail.com>
2018-04-03 22:34:05 +0200

    run all tests on build and update readme file

--- app/build.sh
+++ app/build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-cd ../docker && docker-compose up -d workspace
+cd docker && docker-compose up -d workspace
 sleep 1
 docker-compose exec workspace composer global require hirak/prestissimo
 docker-compose exec workspace composer install
@@ -8,3 +8,6 @@ docker-compose up -d mysql
 sleep 3
 docker-compose exec workspace php artisan migrate:refresh --seed
 docker-compose up -d nginx redis phpmyadmin
+sleep 2
+docker-compose exec workspace ./vendor/bin/phpunit tests/Feature
+echo "Finished. Please follow instructions from Installation guide on Github"
